c6465c086282bdd1f1ce9089b7ddfcac223cac88
Eduba <55287755+edubaschool@users.noreply.github.com>
2020-05-22 23:38:05 +0200

    new bookmarklet

--- facebook-group-scraper.js
+++ facebook-group-scraper.js
@@ -1,15 +1,28 @@
+var arr = [];
+
+String.prototype.fakeReplace = function (str, newstr) {
+  return this.split(str).join(newstr)
+}
+
 var html = document.body.innerHTML;
 var regex = /rel="dialog"\stitle="[A-Z]\S+\s\S+?(?=\")/g;
 var found = html.match(regex);
 
-var i;
+var i
 for (i = 0; i < found.length; i++) {
-  to_replace = found[i];
-    elem_replaced = to_replace.replace('rel="dialog" title="', "")
-    console.log(elem_replaced)
+  to_replace = found[i]
+  elem_replaced = to_replace.fakeReplace('rel="dialog" title="', '')
+  verif_in_arr = arr.includes(elem_replaced);
+  if (verif_in_arr === false) {
+    arr.push(elem_replaced);
+  }
+}
+  
+var e;
+for (e = 0; e < arr.length; e++){
+  document.write("<p>" + arr[e] + "</p>");
 }
 
-//THIS IS THE BOOKMARKLET GENERATED ON : https://mrcoles.com/bookmarklet/
+//THIS IS THE BOOKMARKLET GENERATED ON : https://bookmarklets.org/maker/
 
-//javascript:(function()%7Bvar%20html%20%3D%20document.body.innerHTML%3Bvar%20regex%20%3D%20%2Frel%3D%22dialog%22%5Cstitle%3D%22%5BA-Z%5D%5CS%2B%5Cs%5CS%2B%3F(%3F%3D%5C%22)%2Fg%3Bvar%20found%20%3D%20html.match(regex)%3Bvar%20i%3Bfor%20(i%20%3D%200%3B%20i%20%3C%20found.length%3B%20i%2B%2B)%20%7Bto_replace%20%3D%20found%5Bi%5D%3Belem_replaced%20%3D%20to_replace.replace('rel%3D%22dialog%22%20title%3D%22'%2C%20%22%22)console.log(elem_replaced)%7D%7D)()
-
+//javascript:void%20function(){var%20e=[];String.prototype.fakeReplace=function(e,r){return%20this.split(e).join(r)};var%20r,l=document.body.innerHTML,t=/rel=%22dialog%22\stitle=%22[A-Z]\S+\s\S+%3F(%3F=\%22)/g,i=l.match(t);for(r=0;r%3Ci.length;r++)to_replace=i[r],elem_replaced=to_replace.fakeReplace('rel=%22dialog%22%20title=%22',%22%22),verif_in_arr=e.includes(elem_replaced),verif_in_arr===!1%26%26e.push(elem_replaced);var%20a;for(a=0;a%3Ce.length;a++)document.write(%22%3Cp%3E%22+e[a]+%22%3C/p%3E%22)}();
