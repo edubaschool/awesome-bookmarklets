6b32fc5afb9b8d726e058f53a0b992d488a82e5c
Eduba <55287755+edubaschool@users.noreply.github.com>
2020-05-28 23:17:38 +0200

    changement des balises p par une balise br
    J'ai effectué ce changement pour faciliter la sauvegarde et la copie des urls

--- url-scraper.js
+++ url-scraper.js
@@ -5,8 +5,8 @@ var found = html.match(regex);
 
 var i;
 for (i = 0; i < found.length; i++) {
-  document.write("<p>" + found[i] + "</p>")
+  document.write(found[i] + "</br>")
 }
 
 // Bookmarklet generated
-// javascript:(function()%7Bvar%20html%20%3D%20document.body.innerHTML%3Bvar%20regex%20%3D%20%2Fhttps%3A%5C%2F%5C%2F%5B%5Cw%5C.%5C%2F%5C-%26%3D%5C%25%5D%2B%2Fg%3Bvar%20found%20%3D%20html.match(regex)%3Bvar%20i%3Bfor%20(i%20%3D%200%3B%20i%20%3C%20found.length%3B%20i%2B%2B)%20%7Bdocument.write(%22%3Cp%3E%22%20%2B%20found%5Bi%5D%20%2B%20%22%3C%2Fp%3E%22)%7D%7D)()
+// javascript:(function()%7Bvar%20html%20%3D%20document.body.innerHTML%3Bvar%20regex%20%3D%20%2Fhttps%3A%5C%2F%5C%2F%5B%5Cw%5C.%5C%2F%5C-%26%3D%5C%25%5D%2B%2Fg%3Bvar%20found%20%3D%20html.match(regex)%3Bvar%20i%3Bfor%20(i%20%3D%200%3B%20i%20%3C%20found.length%3B%20i%2B%2B)%20%7Bdocument.write(found%5Bi%5D%20%2B%20%22%3C%2Fbr%3E%22)%7D%7D)()
